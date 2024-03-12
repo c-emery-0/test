3923e5a8e9e06da4c7460da5edc0f5f785a29b3e
celeste emery <88988526+c-emery-0@users.noreply.github.com>
2024-03-12 17:33:15 +0000

    finally something fucking works

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,18 +1,94 @@
-import React, { useState } from "react";
-import { DndContainerTest } from "./dnd-container-test";
-import "./App.css";
+import "./index.css";
+import {
+  DndContext,
+  useSensor,
+  useSensors,
+  PointerSensor,
+  KeyboardSensor,
+  closestCenter
+} from "@dnd-kit/core";
 
-function App() {
-  const items = {
-    group1: ["1", "2", "3"],
-    group2: ["4", "5", "6"],
-    group3: ["7", "8", "9"],
+import {
+  useSortable,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+  arrayMove
+} from "@dnd-kit/sortable";
+import { restrictToVerticalAxis } from "@dnd-kit/modifiers";
+import { useState } from "react";
+import { CSS } from "@dnd-kit/utilities";
+
+function SortableItem(props) {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition
+  } = useSortable({ id: props.id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition
   };
 
-
   return (
-    <DndContainerTest items={items}/>
+    <div
+      className="border bg-sky-200 my-2"
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
+    >
+      {props.id}
+    </div>
   );
 }
 
-export default App;
+export default function App() {
+  const [items, setItems] = useState(["1", "2", "3", "4", "5"]);
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates
+    })
+  );
+
+  return (
+    <div className="p-5">
+      <div className="w-[300px]">
+        <DndContext
+          sensors={sensors}
+          onDragEnd={handleDragEnd}
+          collisionDetection={closestCenter}
+          modifiers={[restrictToVerticalAxis]}
+        >
+          <div className="border">
+            <SortableContext
+              items={items}
+              strategy={verticalListSortingStrategy}
+            >
+              {items.map((id) => (
+                <SortableItem key={id} id={id} />
+              ))}
+            </SortableContext>
+          </div>
+        </DndContext>
+      </div>
+    </div>
+  );
+
+  function handleDragEnd(event) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
+}
